5235fa9066fe271f76b26f3c9acc179b7d770ebf
Ron Mac <ronald@treeboxsolutions.com>
2021-05-07 10:26:21 +0800

    Corrections

--- calcLength.py
+++ calcLength.py
@@ -7,14 +7,14 @@ t_emoji = json.loads(f.read())
 
 print("Grapheme LEN |    LEN    |   LEN (UTF8)   | System Size in Bytes")
 
-
-l1 = gr.length(x)
-l2 = len(x)
-l3 = len(x.encode("utf8"))
-l4 = sys.getsizeof(x)
-
 for key in t_emoji:
-
+ 
     x = t_emoji[key]
+
+    l1 = gr.length(x)
+    l2 = len(x)
+    l3 = len(x.encode("utf8"))
+    l4 = sys.getsizeof(x)
+
     print( key )
     print(  x, "\n", l1, "\t", l2, "\t", l3, "\t", l4, "\n" )
